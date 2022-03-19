bfd38a9c987fa0d826a47d514fea25a5b52a2c1b
lianlian6 <101176182+lianlian6@users.noreply.github.com>
2022-03-19 13:57:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-	"github.com/Sansui233/proxypoolCheck/api"
-	"github.com/Sansui233/proxypoolCheck/config"
-	"github.com/Sansui233/proxypoolCheck/internal/app"
-	"github.com/Sansui233/proxypoolCheck/internal/cron"
+	"github.com/Sansui233/proxypoolCheck/tree/master/api"
+	"github.com/Sansui233/proxypoolCheck/tree/master/config"
+	"github.com/Sansui233/proxypoolCheck/tree/master/internal/app"
+	"github.com/Sansui233/proxypoolCheck/tree/master/internal/cron"
 	"log"
 	"net/http"
 )
